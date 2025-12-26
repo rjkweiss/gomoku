8db86ccc3d9af7a534636384bc61ba52283cf54e
Joyce Kimojino-Weiss <jkimojinoweiss@gmail.com>
2025-12-26 17:42:44 -0600

    update prisma requirements

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/Prisma/client.js';
+import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg'
 
 const connectionString = process.env.DATABASE_URL!;
