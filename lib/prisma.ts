7e9e398a52869b3d8b61ca0b27720ded3b493b61
Joyce Kimojino-Weiss <jkimojinoweiss@gmail.com>
2025-12-26 18:10:44 -0600

    updating serverless render for vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,18 @@
 import 'dotenv/config';
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg'
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
+}
 
-// const connectionString = process.env.DATABASE_URL!;
-// const pool = new Pool({ connectionString });
-// const adapter = new PrismaPg(pool);
-// const prisma = new PrismaClient({ adapter });
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ['error']
+    })
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma
+}
 
 export default prisma;
